f12e3b4ec27193024c5ed605bea043078b47f70a
JordiMateoUdL <jordi.mateo@udl.cat>
2025-02-07 14:59:30 +0100

    fix: maths

--- js/custom-mathjax.js
+++ js/custom-mathjax.js
@@ -1,5 +1,9 @@
-window.MathJax = {
-  tex2jax: {
+MathJax.Hub.Config({
+  TeX: {
     inlineMath: [['\\(', '\\)'], ['$', '$']]
+  },
+  skipTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+  tex2jax: {
+    inlineMath: [['\\(', '\\)'], ['$', '$']],
   }
-};
+});
